c2a7c2951a559d685395827e94ad31bb607c04f0
masterblaster42069 <114529314+masterblaster42069@users.noreply.github.com>
2024-01-08 10:54:34 +0100

    Add project files.

---
+++ Higher or lower/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Higher_or_lower
+{
+    internal class GuessingGame
+    {
+        private Random rand = new Random();
+
+        public void StartGame()
+        {
+            while (true) 
+            { 
+                int num = rand.Next(1, 101);
+                Console.WriteLine("Guess if the number is higher or lower than 50. Type '>' for higher and '<' for lower.");
+
+                string input = Console.ReadLine();
+                while (input != ">" && input != "<") 
+                {
+                    Console.WriteLine("Invalid input. Please type '>' for higher or '<' for lower.");
+                    input = Console.ReadLine();
+                }
+
+                if ((num > 50 && input == ">") || (num < 50 && input == "<"))
+                {
+                    Console.WriteLine($"You are correct, the number was {num}.");
+                }
+                else if (num == 50)
+                {
+                    Console.WriteLine($"It's a draw! The number was exactly 50.");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, you are wrong. The number was {num}.");
+                }
+            }
+        }
+    }
+}
---
+++ Higher or lower/Program.cs
@@ -0,0 +1,9 @@
+﻿using Higher_or_lower;
+class Program
+{
+    static void Main()
+    {
+        GuessingGame game = new GuessingGame();
+        game.StartGame();
+    }
+}
